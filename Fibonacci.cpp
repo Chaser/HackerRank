652869c857faaa4b00f12fe9123930d559be7360
Chase Johnson <c.johnson@ortolife.com>
2014-02-14 12:59:15 +1000

    Fibonacci template creation

---
+++ Fibonacci.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int cases;
+  cin >> cases;
+  while (cases--) {
+    // Code here!
+  }
+}
---
+++ Fibonacci.py
@@ -0,0 +1,8 @@
+import sys
+
+def run():
+  # Code here!
+  pass
+
+for i in range(int(sys.stdin.readline())):
+  run()
