68e70466f857f8179c3af37ec142f0b1948bdead
Chase Johnson <c.johnson@ortolife.com>
2014-02-14 14:20:17 +1000

    Fibonacci python solution

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,8 +1,17 @@
 import sys
 
 def run():
-  # Code here!
-  pass
+	for x in range(int(sys.stdin.readline())):
+		print fib(x)
+
+def fib(n):
+	if(n==0):
+		return 0;
+	elif(n==1 or n == 2):
+		return 1;
+	else:
+		return fib(n-2) + fib(n-1)
+
 
 for i in range(int(sys.stdin.readline())):
   run()
