bc9748934d29ddf4143d897c90073a96516159a8
Chase Johnson <c.johnson@ortolife.com>
2014-02-14 13:45:31 +1000

    C++ solution

--- Fibonacci.cpp
+++ Fibonacci.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
 using namespace std;
+int fib(int);
 int main() {
   int cases;
+  int x = 0;
   cin >> cases;
+  cin >> x;
   while (cases--) {
-    // Code here!
+  	for (int i=0;i<=x-1;i++) {
+  		cout << fib(i) << '\n';
+  	}
   }
+}
+
+int fib(int n) {
+	if(n==0)
+		return 0;
+	if(n==1 || n==2)
+		return 1;
+	else
+	{
+		return (fib(n-2) + fib(n-1));
+	}
+
 }
